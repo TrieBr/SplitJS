8a24e9ce28f1947a8cc3a28820b60c39df685a15
Brad <triebr@gmail.com>
2015-03-12 14:10:57 -0600

    Converted to jquery plugin

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
-Split JS is a small js library that allows you to split an html element vertically or horizontally with a handle to adjust the size.
+Split JS is a small jQuery plugin that allows you to split an html element vertically or horizontally with a handle to adjust the size.
+
+How to use:
+
+    <div id="split1">
+                    <div class="split-component-1" style="width: 100px;">
+                	    First Half
+                    </div>
+                    <div class="split-component-2">
+                	    Second Half
+                    </div>
+    </div>
+
+
+Then use:
+
+    $("#split1").splitIt("vertical");
 
 Visit the github page for a demo [here](http://triebr.github.io/SplitJS/demo.html)
+
--- split.js
+++ split.js
@@ -1,64 +1,63 @@
 var isDragging = false;
 var splitPaneDragging;
 
-
-
-//Initialize split panes
-//Author: Brad Triebwasser
-function initializeSplitPanes() {
-    
-    $(window).resize(function(e) { //Make sure the fill elements resize when the window resizes
-        setFillElements();
-    });
-
-    //Vertical split pane Handle grabbed (mouse down event)
-   $(".split-pane-vertical").children(".split-handle").mousedown(function(event) {
-    console.log("Dragging begin on handle");
-    var parentSplitPane = $(this).parent();
-    parentSplitPane.data("mouseStartX",event.pageX);
-    parentSplitPane.data("originalWidth",parentSplitPane.find(".split-component-1").width());
-    var splitPaneDragging = parentSplitPane;
-        $(window).mousemove(function( event ) {
-                isDragging = true;
-                var splitComponent1 = splitPaneDragging.children(".split-component-1");
-                splitComponent1.width(parentSplitPane.data("originalWidth")+(event.pageX-parentSplitPane.data("mouseStartX")));
-                setFillElements()
-                if(event.preventDefault) event.preventDefault();
-        });
-       if(event.preventDefault) event.preventDefault();
-    });
-
-   //Horizontal split pane Handle grabbed (mouse down event)
-   $(".split-pane-horizontal").children(".split-handle").mousedown(function(event) {
-    console.log("Dragging begin on handle Hor");
-    var parentSplitPane = $(this).parent();
-    parentSplitPane.data("mouseStartY",event.pageY);
-    parentSplitPane.data("originalHeight",parentSplitPane.find(".split-component-1").height());
-    var splitPaneDragging = parentSplitPane;
-        $(window).mousemove(function( event ) {
-                isDragging = true;
-                var splitComponent1 = splitPaneDragging.children(".split-component-1");
-                splitComponent1.height(parentSplitPane.data("originalHeight")+(event.pageY-parentSplitPane.data("mouseStartY")));
-                if (splitComponent1.height()>=splitPaneDragging.height()-32) {
-                    splitComponent1.height(splitPaneDragging.height()-32);
-                }
-                setFillElements() 
-               
-
-                if(event.preventDefault) event.preventDefault();
-        });
-       if(event.preventDefault) event.preventDefault();
-    });
-
-
-   $(window).mouseup(function() { //Unbind mousemove event when the mouse is release (ie. finished dragging the handle)
+function splitjsInit() {
+    $(window).mouseup(function() { //Unbind mousemove event when the mouse is release (ie. finished dragging the handle)
         if (isDragging) {
             $(window).unbind("mousemove");
         }
     });
-
-   setFillElements();
 }
+
+$.fn.splitIt = function(direction) {
+    splitjsInit();
+    if (direction=="vertical") {
+        $(this).addClass("split-pane-vertical");
+        var handle = $('<div class="split-handle"></div>');
+        $(this).children(".split-component-1").after(handle);
+        $(handle).mousedown(function(event) {
+        var parentSplitPane = $(this).parent();
+        parentSplitPane.data("mouseStartX",event.pageX);
+        parentSplitPane.data("originalWidth",parentSplitPane.find(".split-component-1").width());
+        var splitPaneDragging = parentSplitPane;
+            $(window).mousemove(function( event ) {
+                    isDragging = true;
+                    var splitComponent1 = splitPaneDragging.children(".split-component-1");
+                    splitComponent1.width(parentSplitPane.data("originalWidth")+(event.pageX-parentSplitPane.data("mouseStartX")));
+                    setFillElements()
+                    if(event.preventDefault) event.preventDefault();
+            });
+           if(event.preventDefault) event.preventDefault();
+        });
+    }else{
+        $(this).addClass("split-pane-horizontal");
+        //Horizontal split pane Handle grabbed (mouse down event)
+        var handle = $('<div class="split-handle"></div>');
+         $(this).children(".split-component-1").after(handle);
+       $(handle).mousedown(function(event) {
+        var parentSplitPane = $(this).parent();
+        parentSplitPane.data("mouseStartY",event.pageY);
+        parentSplitPane.data("originalHeight",parentSplitPane.find(".split-component-1").height());
+        var splitPaneDragging = parentSplitPane;
+            $(window).mousemove(function( event ) {
+                    isDragging = true;
+                    var splitComponent1 = splitPaneDragging.children(".split-component-1");
+                    splitComponent1.height(parentSplitPane.data("originalHeight")+(event.pageY-parentSplitPane.data("mouseStartY")));
+                    if (splitComponent1.height()>=splitPaneDragging.height()-6) {
+                        splitComponent1.height(splitPaneDragging.height()-6);
+                    }
+                    setFillElements() 
+
+
+                    if(event.preventDefault) event.preventDefault();
+            });
+           if(event.preventDefault) event.preventDefault();
+        });
+        
+    }
+    setFillElements();
+};
+
 
 
 //Sets elements filling a split component to match its dimensions
